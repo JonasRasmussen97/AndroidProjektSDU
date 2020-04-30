a4e274228e6f02fccfa8b4f0a1c71e8208c3f5a8
Jonas <Saltogpeber123@hotmail.com>
2020-04-30 15:58:02 +0200

    Started working on RecycleView

--- InterCareApp/app/src/main/java/com/example/intercareapp/MainActivity.java
+++ InterCareApp/app/src/main/java/com/example/intercareapp/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.intercareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView organizationsRecycleView;
+    private ArrayList<String> organizationNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        organizationNames = new ArrayList();
+        organizationNames.add("Bobs Juice Bar");
+        organizationNames.add("Rollos Bokseklub");
+        organizationNames.add("De fem fede sømænd");
+
+        this.organizationsRecycleView = findViewById(R.id.organizationsRecycleView);
+        MyAdapter myAdapter = new MyAdapter(this, this.organizationNames);
+        this.organizationsRecycleView.setAdapter(myAdapter);
+        this.organizationsRecycleView.setLayoutManager(new LinearLayoutManager(this));
+
     }
+
+
+
 }
---
+++ InterCareApp/app/src/main/java/com/example/intercareapp/MyAdapter.java
@@ -0,0 +1,62 @@
+package com.example.intercareapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<String> organizationNamesList;
+
+
+    public MyAdapter(Context context, ArrayList<String> organizationNamesList) {
+        this.context = context;
+        this.organizationNamesList = organizationNamesList;
+    }
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    holder.organizationNameText.setText(organizationNamesList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return organizationNamesList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView organizationNameText;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            organizationNameText = itemView.findViewById(R.id.organizationName);
+            itemView.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+}
